6c0172c1990c8a6a65341b04ead68dc9a523b541
Gustavo <gustavoglu@hotmail.com>
2026-08-18 15:51:40 -0300

    feat(tts): add persistent overlay tables

--- src/MyStreamBot.Infrastructure/Persistence/MyStreamBotDbContext.cs
+++ src/MyStreamBot.Infrastructure/Persistence/MyStreamBotDbContext.cs
@@ -3,76 +3,47 @@ using MyStreamBot.Core.Entities;
 
 namespace MyStreamBot.Infrastructure.Persistence;
 
-public sealed class MyStreamBotDbContext(
-    DbContextOptions<MyStreamBotDbContext> options)
-    : DbContext(options)
+public sealed class MyStreamBotDbContext(DbContextOptions<MyStreamBotDbContext> options) : DbContext(options)
 {
-    public DbSet<StreamerUser> Users =>
-        Set<StreamerUser>();
+    public DbSet<StreamerUser> Users => Set<StreamerUser>();
+    public DbSet<PointTransaction> PointTransactions => Set<PointTransaction>();
+    public DbSet<TtsOverlayEvent> TtsOverlayEvents => Set<TtsOverlayEvent>();
+    public DbSet<TtsOverlayClient> TtsOverlayClients => Set<TtsOverlayClient>();
 
-    public DbSet<PointTransaction> PointTransactions =>
-        Set<PointTransaction>();
-
-    protected override void OnModelCreating(
-        ModelBuilder modelBuilder)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-
         modelBuilder.Entity<StreamerUser>(e =>
         {
             e.HasKey(x => x.Id);
-
-            e.HasIndex(x =>
-                new
-                {
-                    x.Platform,
-                    x.PlatformUserId
-                })
-                .IsUnique();
-
-            e.Property(x => x.Username)
-                .HasMaxLength(100)
-                .IsRequired();
-
-            e.Property(x => x.PlatformUserId)
-                .HasMaxLength(200)
-                .IsRequired();
-
-            // Avatar do usuário.
-            // Pode ser nulo porque algumas plataformas/
-            // mensagens podem não fornecer avatar.
-            e.Property(x => x.AvatarUrl)
-                .HasMaxLength(1000)
-                .IsRequired(false);
-
-            e.Property(x => x.LastChatMessageNormalized)
-                .HasMaxLength(4000)
-                .IsRequired(false);
+            e.HasIndex(x => new { x.Platform, x.PlatformUserId }).IsUnique();
+            e.Property(x => x.Username).HasMaxLength(100).IsRequired();
+            e.Property(x => x.PlatformUserId).HasMaxLength(200).IsRequired();
+            e.Property(x => x.AvatarUrl).HasMaxLength(1000).IsRequired(false);
+            e.Property(x => x.LastChatMessageNormalized).HasMaxLength(4000).IsRequired(false);
         });
-
         modelBuilder.Entity<PointTransaction>(e =>
         {
             e.HasKey(x => x.Id);
-
-            e.HasIndex(x =>
-                new
-                {
-                    x.UserId,
-                    x.CreatedAtUtc
-                });
-
-            e.HasIndex(x => x.SourceMessageKey)
-                .IsUnique();
-
-            e.Property(x => x.SourceMessageKey)
-                .HasMaxLength(4096)
-                .IsRequired(false);
-
-            e.HasOne(x => x.User)
-                .WithMany()
-                .HasForeignKey(x => x.UserId)
-                .OnDelete(
-                    DeleteBehavior.Cascade);
+            e.HasIndex(x => new { x.UserId, x.CreatedAtUtc });
+            e.HasIndex(x => x.SourceMessageKey).IsUnique();
+            e.Property(x => x.SourceMessageKey).HasMaxLength(4096).IsRequired(false);
+            e.HasOne(x => x.User).WithMany().HasForeignKey(x => x.UserId).OnDelete(DeleteBehavior.Cascade);
+        });
+        modelBuilder.Entity<TtsOverlayEvent>(e =>
+        {
+            e.HasKey(x => x.Id);
+            e.Property(x => x.Username).HasMaxLength(100).IsRequired();
+            e.Property(x => x.AvatarUrl).HasMaxLength(1000).IsRequired(false);
+            e.Property(x => x.Text).HasMaxLength(4000).IsRequired();
+            e.Property(x => x.AudioFileName).HasMaxLength(500).IsRequired();
+            e.HasIndex(x => x.CreatedAtUtc);
+        });
+        modelBuilder.Entity<TtsOverlayClient>(e =>
+        {
+            e.HasKey(x => x.Id);
+            e.HasIndex(x => x.ClientName).IsUnique();
+            e.Property(x => x.ClientName).HasMaxLength(200).IsRequired();
         });
     }
 }
